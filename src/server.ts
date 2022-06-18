e88c23092a06e84fe5b2596be5b11c317c75c061
TMMunyai <33489196+TMMunyai@users.noreply.github.com>
2022-06-18 20:35:43 +0200

    build and dev mod

--- src/server.ts
+++ src/server.ts
@@ -13,25 +13,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage/", async ( req, res ) => {
-    let image_url = req.query;
-    if (image_url) {
-      filterImageFromURL(image_url).then((response)=> {
+  app.get("/filteredimage", async ( req, res ) => {
+    let url = req.query.image_url;
+    try {if (url !== "") {
+      await filterImageFromURL(url).then((response)=> {
         res.sendFile(response);
         res.on('Completed, now deleting response', function() {
           deleteLocalFiles([response]);
         });
       });
-    } else {
-      res.status(404).send("The URL is not True");
-    }
+    } else {res.status(404).send("The URL cannot found");
+    }}catch (Exception){res.status(404).send("The URL can not Found");}
   });
   
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  // // Root Endpoint
+  // // Displays a simple message to the user
+  // app.get( "/", async ( req, res ) => {
+  //   res.send("try GET /filteredimage?image_url={{}}")
+  // } );
   
 
   // Start the Server
